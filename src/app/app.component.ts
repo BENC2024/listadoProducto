f77065bbf55c1022ead47472a483eec8d6d49187
brahian github <brahiangithub@gmail.com>
2023-11-07 15:39:59 -0500

    creando estructura

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'listado';
+   title = 'angular-course';
+
+   productos = [
+      {id: 100, nombre: "arroz", precio: 1500, descripcion: 'AAA',  Fecha_Expedicion: '20-10-2023', Fecha_vencimiento: '2023-12-01'},
+      {id: 101, nombre: "azucar", precio: 2000, descripcion: 'BBB',  Fecha_Expedicion: '20-10-2023', Fecha_vencimiento: '2023-12-08'},
+      {id: 102, nombre: "cafe", precio: 2000, descripcion: 'CCC',  Fecha_Expedicion: '20-10-2023', Fecha_vencimiento: '2023-12-12'},
+      {id: 103, nombre: "sal", precio: 1000, descripcion: 'DDD',  Fecha_Expedicion: '20-10-2023', Fecha_vencimiento: '2023-12-10'},
+      {id: 104, nombre: "harina", precio: 1800, descripcion: 'EEE',  Fecha_Expedicion: '20-10-2023', Fecha_vencimiento: '2023-12-13'},
+      {id: 105, nombre: "pollo", precio: 9200, descripcion: 'FFF',  Fecha_Expedicion: '20-10-2023', Fecha_vencimiento: '2023-12-17'},
+   ];
+
+   codigo = "";
+
+   buscarProducto(){
+      return this.productos
+   }
+
 }
